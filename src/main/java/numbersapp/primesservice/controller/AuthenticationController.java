d3643056117ec94c0e1300af21dab3bf047489e4
Dunna, Kranthi Kumar <kranthiwork08@gmail.com>
2024-03-26 22:55:11 -0400

    latestv3

--- src/main/java/numbersapp/primesservice/controller/AuthenticationController.java
+++ src/main/java/numbersapp/primesservice/controller/AuthenticationController.java
@@ -1,6 +1,5 @@
 package numbersapp.primesservice.controller;
 
-
 import numbersapp.primesservice.model.Customer;
 import numbersapp.primesservice.service.AuthenticationService;
 import numbersapp.primesservice.service.IAuthenticationService;
@@ -15,39 +14,38 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import java.io.IOException;
 
 @RestController
 public class AuthenticationController {
 
-    private final IAuthenticationService iAuthenticationService ;
+    private final IAuthenticationService iAuthenticationService;
     private final AuthenticationManager authenticationManager;
 
     private final TokenService tokenService;
 
-    public AuthenticationController(IAuthenticationService iAuthenticationService, AuthenticationManager authenticationManager, TokenService tokenService) {
+    public AuthenticationController(IAuthenticationService iAuthenticationService,
+            AuthenticationManager authenticationManager, TokenService tokenService) {
         this.iAuthenticationService = iAuthenticationService;
         this.authenticationManager = authenticationManager;
         this.tokenService = tokenService;
     }
 
     @PostMapping("/register")
-    public boolean register(@RequestBody Customer customer){
-        try{
+    public boolean register(@RequestBody Customer customer) {
+        try {
             return iAuthenticationService.register(customer);
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody Customer customer){
+    public String login(@RequestBody Customer customer) {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
-                        customer.getUsername(),customer.getPassword()
-                )
-        );
+                        customer.getUsername(), customer.getPassword()));
         return tokenService.generateToken(authentication);
     }
 }
